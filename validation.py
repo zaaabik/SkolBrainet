47981b401a8d37220421fcdbfafea9b09acf2cdc
kechua <kechua98@gmail.com>
2020-03-28 01:05:27 +0300

    added file for the experiment of testing the model across dataset

--- validation.py
+++ validation.py
@@ -76,4 +76,6 @@ full_predict = predict_full(net, padded_img, crop_size=crop_size, mini_crop_size
                             thr=0.5,
                             step_size=7,
                             device=device)
+
+np.save('test_predict.npy', full_predict)
 print(dc(full_predict, padded_gt))
