9419854854b110100470c8d0d1460758514a5b1b
zabik <zaaabik@gmail.com>
2020-04-03 20:13:18 +0300

    fixed Dataset bug

--- Dataset.py
+++ Dataset.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 from torch.utils import data
 
 
@@ -48,6 +49,55 @@ class MriDataset(data.Dataset):
         crop = self.crops[idx]
         img = self.X[self.img_idxs[idx]]
         gt = self.y[self.img_idxs[idx]]
-        x, y = self.crop_function(img, gt,
+        x, y = self.crop_function(img, gt, self.crop_size, self.mini_crop_size, crop)
 
         return torch.Tensor(x[None, :, :, :]), torch.Tensor(y[None, :, :, :])
+
+
+class MriDatasetWithDomain(data.Dataset):
+    def __init__(self, X, y, cat, crop_size, mini_crop_size, crops_per_image, crop_function):
+        super(MriDataset)
+        self.X = [x / x.max() for x in X]
+        self.y = y
+        self.cat = cat
+        self.crop_size = crop_size
+        self.mini_crop_size = mini_crop_size
+        self.crops_per_image = crops_per_image
+        self.crops_per_image = crops_per_image
+        self.crop_function = crop_function
+        self.crops, self.img_idxs, self.img_cats = self.create_crops()
+
+    def create_crops(self):
+        crops = []
+        img_idxs = []
+        img_cats = []
+
+        for i in range(len(self.X)):
+            x_shape = np.array(self.X[i].shape)
+
+            for j in range(self.crops_per_image):
+                x_max = x_shape[0] - self.crop_size
+                y_max = x_shape[1] - self.crop_size
+                z_max = x_shape[2] - self.crop_size
+
+                x = np.random.randint(0, x_max)
+                y = np.random.randint(0, y_max)
+                z = np.random.randint(0, z_max)
+
+                crops.append([x, y, z])
+                img_idxs.append(i)
+                img_cats.append(self.cat[i])
+
+        return crops, img_idxs, img_cats
+
+    def __len__(self):
+        return len(self.X) * self.crops_per_image
+
+    def __getitem__(self, idx):
+        crop = self.crops[idx]
+        img = self.X[self.img_idxs[idx]]
+        gt = self.y[self.img_idxs[idx]]
+        cat = self.img_cats[idx]
+        x, y = self.crop_function(img, gt, self.crop_size, self.mini_crop_size, crop)
+
+        return torch.Tensor(x[None, :, :, :]), torch.Tensor(y[None, :, :, :]), np.array(cat, dtype=np.int64)
